74a9141af52be641297d2a1b4daf742cb65a7697
Rohermy Ochoa <rohermy.ochoa@gmail.com>
2023-04-25 09:55:29 -0400

    fixing some bugs

--- script.js
+++ script.js
@@ -1,41 +1,43 @@
 import puppeteer from 'puppeteer'
 
 export default async function bcvScraping() {
-  let browser, page
-
   try {
-    browser = await puppeteer.launch()
-    page = await browser.newPage()
+    const browser = await puppeteer.launch()
+    const page = await browser.newPage()
+
+    async function getPageData() {
+      await page.goto('http://bcv.org.ve')
+      await page.waitForSelector('#dolar')
+      const data = await page.$$eval('.centrado', textData => {
+        textData = textData.map(el => el.querySelector('strong').textContent.replace(',', '.'))
+        console.log(textData)
+        dataNumber = textData.map(el => Number.parseFloat(el).toFixed(2))
+        return dataNumber
+      })
+
+      const exchangeRate = {
+        eur: data[0],
+        cny: data[1],
+        try: data[2],
+        rub: data[3],
+        usd: data[4],
+      }
+
+      return exchangeRate
+    }
+
+    const exchange = await getPageData()
+    await browser.close()
+    // aqui se retorna el objeto con los datos de las tasas de cambio del BCV
+    return exchange
+
   }catch (err) {
     throw new Error('could not create a browser instance => : ', err)
   }
-
-  async function getPageData() {
-    await page.goto('http://bcv.org.ve')
-    await page.waitForSelector('#dolar')
-    let data = await page.$$eval('.centrado', textData => {
-      textData = textData.map(el => el.querySelector('strong').textContent.replace(',', '.'))
-      console.log(textData)
-      dataNumber = textData.map(el => Number.parseFloat(el).toFixed(2))
-      return dataNumber
-    })
-
-    let exchangeRate = {
-      eur: data[0],
-      cny: data[1],
-      try: data[2],
-      rub: data[3],
-      usd: data[4],
-    }
-
-    return exchangeRate
-  }
-
-  let exchange = await getPageData()
-  await browser.close()
-  //aqui se retorna el objeto con los datos de las tasas de cambio del BCV
-  return exchange
 }
 
 //funcion que recupera los datos
-console.log(await bcvScraping())
+
+bcvScraping()
+  .then(exchange => console.log(exchange))
+  .catch(err => console.log(err))
